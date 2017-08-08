d107a16b6964bad7954311097e69a96e75c13bd6
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-08 13:13:19 -0400

    Update ConnectionTrait.php

--- ConnectionTrait.php
+++ ConnectionTrait.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Doctrine DBAL Util package.
+ *
+ * (c) Jean-Bernard Addor
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace DoctrineDbalUtil\Connection;
 
 use Doctrine\DBAL\Driver\Connection;
@@ -10,10 +19,14 @@ trait ConnectionTrait
 
     private $dbal;
 
-    public function __construct(Connection $dbal) {
-       $this->dbal = $dbal;
-       // parent::__construct();
+    public function __construct(Connection $dbal)
+    {
+        $this->dbal = $dbal;
+        // parent::__construct();
     }
 
-    protected function getConnection() { return $this->dbal; }
+    protected function getConnection()
+    {
+        return $this->dbal;
+    }
 }
