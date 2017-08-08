2231b512f8444dd0a7cceb91bca24d5a161b0c3a
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-08 13:08:03 -0400

    Update ConnectionAbstractTrait.php

--- ConnectionAbstractTrait.php
+++ ConnectionAbstractTrait.php
@@ -1,8 +1,15 @@
 <?php
 
-namespace DoctrineDbalUtil\Connection;
+/*
+ * This file is part of the Doctrine DBAL Util package.
+ *
+ * (c) Jean-Bernard Addor
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-use Doctrine\DBAL\Driver\Connection;
+namespace DoctrineDbalUtil\Connection;
 
 trait ConnectionAbstractTrait // TODO: should this trait itself be made abstract?
 {
